244938bcdae32f5b0a03525257d248b710d3d821
Jeroen production <jeroenooms@gmail.com>
2011-10-03 18:43:04 -0700

    biplot fixes

--- R/multifactor.R
+++ R/multifactor.R
@@ -37,3 +37,44 @@ expand.multifactor <- function(x){
 dim.multifactor <- function(x){
 	return(sapply(strsplit(x, "+", fixed=TRUE), length));
 }
+
+is.multifactor <- function(x){
+	if("multifactor" %in% class(x)) {
+		return(TRUE)
+	} else {
+		return(FALSE);
+	}
+}
+
+"[.multifactor" <- function(x, ..., drop = FALSE){
+	y <- NextMethod("[")
+	attr(y, "labels") <- attr(x, "labels")
+	attr(y, "levels") <- attr(x, "levels")
+	attr(y, "ordered") <- attr(x, "ordered")
+	attr(y, "prompt_type") <- attr(x, "prompt_type")
+	class(y) <- oldClass(x)
+	lev <- levels(x)
+	if (drop) 
+		factor(y, exclude = if (any(is.na(levels(x)))) 
+			NULL
+		else NA)
+	else y	
+}
+
+"[[.multifactor" <- function (x, ...) {
+	y <- NextMethod("[[")
+	attr(y, "labels") <- attr(x, "labels")
+	attr(y, "levels") <- attr(x, "levels")
+	attr(y, "ordered") <- attr(x, "ordered")
+	attr(y, "prompt_type") <- attr(x, "prompt_type")
+	class(y) <- oldClass(x)
+	y
+}
+
+rep.multifactor <- function (x, ...) 
+{
+	y <- NextMethod()
+	structure(y, class = class(x), levels = attr(x, "levels"), labels = attr(x, "labels"), ordered=attr(x, "ordered"), prompt_type = attr(x, "prompt_type"));
+}
+
+
